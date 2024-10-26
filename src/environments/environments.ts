330b33f7b06d24952e22a643ad4ec6f2e2d1c940
bombastictranz <bombastictranz@gmail.com>
2024-10-26 13:02:05 +0000

    first commit

---
+++ src/environments/environments.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  firebase: {
+      apiKey: 'AIzaSyB1TZczAaD_WqB9nSHNabTab8DgmaPED0Y',
+      authDomain: 'sample-firebase-ai-app-56843.firebaseapp.com',
+      databaseURL: '',
+      projectId: 'sample-firebase-ai-app-56843',
+      storageBucket: 'sample-firebase-ai-app-56843.appspot.com',
+      messagingSenderId: '29459325846',
+      appId: '1:29459325846:web:0a70911089c9f6cf6673b7',
+      measurementId: '',
+  },
+  gemini_api_key: 'AIzaSyD1GeSwh-CsQtuLqq4Djpoe8ME8C-iO91A',
+  debug_token: ''
+};
